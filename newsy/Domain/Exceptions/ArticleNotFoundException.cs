dc0cf81eb89f0e1292f85a6a3db202585d774c9a
Ivan Golović <ivan.golovic@enghouse.com>
2022-05-08 16:02:01 +0200

    Fix for db creation

--- newsy/Domain/Exceptions/ArticleNotFoundException.cs
+++ newsy/Domain/Exceptions/ArticleNotFoundException.cs
@@ -2,9 +2,9 @@
 
 namespace Domain.Exceptions
 {
-    public sealed class AccountNotFoundException : NotFoundException
+    public sealed class ArticleNotFoundException : NotFoundException
     {
-        public AccountNotFoundException(Guid accountId)
+        public ArticleNotFoundException(Guid accountId)
             : base($"The account with the identifier {accountId} was not found.")    
         {
         }
